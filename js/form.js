417b07ac248d00971703a42b8f95810ddd138ff2
Александр <psychosocial555666@yandex.ru>
2020-03-01 11:54:45 +0700

    Доработано сообщение об успешной отправке

--- js/form.js
+++ js/form.js
@@ -11,7 +11,6 @@
   var effectLevelValue = effectLevelElement.querySelector('.effect-level__value');
   var effectLevelDepth = effectLevelElement.querySelector('.effect-level__depth');
 
-
   var onInputChange = function () {
     imageEditPopup.classList.remove('hidden');
     scaleValue.value = '100%';
@@ -39,6 +38,55 @@
     });
     uploadForm.reset();
   };
+
+  uploadForm.addEventListener('submit', function (evt) {
+    var successTemplate = document.querySelector('#success').content;
+    var main = document.querySelector('main');
+    window.upload(new FormData(uploadForm), function () {
+      imageEditPopup.classList.add('hidden');
+      uploadForm.reset();
+      var successMessage = successTemplate.cloneNode(true);
+      var fragment = document.createDocumentFragment();
+      fragment.appendChild(successMessage);
+      main.appendChild(fragment);
+      var successBtn = document.querySelector('.success__button');
+      var successPlate = document.querySelector('.success');
+      var plateClose = function () {
+        successPlate.remove();
+        successPlate.removeEventListener('click', innerClose);
+        successBtn.removeEventListener('click', plateClose);
+        document.removeEventListener('keydown', function (evtClose) {
+          window.utils.isEscEvent(evtClose, plateClose);
+        });
+        successBtn.removeEventListener('keydown', function (evtClose) {
+          window.utils.isEnterEvent(evtClose, plateClose);
+        });
+      };
+      var innerClose = function (evtInner) {
+        if (evtInner.target && !evtInner.target.matches('.success__inner') && !evtInner.target.matches('.success__title') || evtInner.target.matches('.success__button')) {
+          successPlate.remove();
+          uploadForm.reset();
+          successPlate.removeEventListener('click', innerClose);
+          successBtn.removeEventListener('click', plateClose);
+          document.removeEventListener('keydown', function (evtClose) {
+            window.utils.isEscEvent(evtClose, plateClose);
+          });
+          successBtn.removeEventListener('keydown', function (evtClose) {
+            window.utils.isEnterEvent(evtClose, plateClose);
+          });
+        }
+      };
+      successPlate.addEventListener('click', innerClose);
+      successBtn.addEventListener('click', plateClose);
+      document.addEventListener('keydown', function (evtClose) {
+        window.utils.isEscEvent(evtClose, plateClose);
+      });
+      successBtn.addEventListener('keydown', function (evtClose) {
+        window.utils.isEnterEvent(evtClose, plateClose);
+      });
+    });
+    evt.preventDefault();
+  });
 
   imageInput.addEventListener('change', onInputChange);
 })();
